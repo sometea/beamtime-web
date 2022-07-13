91f8d7eae6b3c7306a0aeeec93cb950107917fa4
David Dasenbrook <david.dasenbrook@tngtech.com>
2022-07-13 19:58:57 +0200

    remove experimental article templates

--- templates/articles.11tydata.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = async function() {
-    const fetchedArticles = await fetch('https://cms.beamtime.net/articles');
-    const articles = await fetchedArticles.json();
-    return { articles };
-}
